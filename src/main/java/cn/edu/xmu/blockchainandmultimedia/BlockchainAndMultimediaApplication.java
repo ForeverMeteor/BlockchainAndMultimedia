ae8dee28590d49deea959a833bf4d2f5c759df8b
ChentaoZhang <2675561402@qq.com>
2023-03-26 18:08:29 +0800

    Baseline

---
+++ src/main/java/cn/edu/xmu/blockchainandmultimedia/BlockchainAndMultimediaApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.blockchainandmultimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainAndMultimediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainAndMultimediaApplication.class, args);
+    }
+
+}
